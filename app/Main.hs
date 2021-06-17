0e8f9932a7dbf02f9f5a2d593384937015409b59
Gabrijel Boduljak <gboduljak@outlook.com>
2021-06-17 21:44:29 +0100

    added trivial repl

--- app/Main.hs
+++ app/Main.hs
@@ -4,8 +4,28 @@ import Ast
 import Evaluator
 import Lib (parseAndEval)
 import Parser
+import System.IO (hFlush, stdout)
 import qualified Text.ParserCombinators.Parsec as Parsec (parse)
 import Text.ParserCombinators.Parsec.Error (ParseError)
 
+flushStr :: String -> IO ()
+flushStr str = putStr str >> hFlush stdout
+
+readPrompt :: String -> IO String
+readPrompt prompt = flushStr prompt >> getLine
+
+evalAndPrint :: String -> IO ()
+evalAndPrint expr = do
+  putStrLn (parseAndEval expr)
+
+repl :: IO ()
+repl = do
+  x <- readPrompt "uscheme >>>"
+  if x == "quit"
+    then return ()
+    else do
+      evalAndPrint x
+      repl
+
 main :: IO ()
-main = getLine >>= putStrLn . parseAndEval
+main = repl
