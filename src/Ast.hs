e7d52c612f6441d9f12fdff5691b8b3bfe82855b
Gabrijel Boduljak <gboduljak@outlook.com>
2021-06-26 21:58:25 +0100

    improved display of lambda

--- src/Ast.hs
+++ src/Ast.hs
@@ -25,7 +25,13 @@ instance Show LispVal where
   show (DottedList xs x) = "(" ++ unwords (map show xs) ++ " . " ++ show x ++ ")"
   show (List xs) = "(" ++ unwords (map show xs) ++ ")"
   show (Vector xs) = "#(" ++ unwords (map show xs) ++ ")"
-  show Lambda {args, body, targetScopeId} = "lambda(" ++ unwords args ++ ")"
+  show Lambda {args, body, varargs, targetScopeId} =
+    "(lambda (" ++ unwords (map show args)
+      ++ ( case varargs of
+             Nothing -> ""
+             Just varArg -> " . " ++ show varArg
+         )
+      ++ ") ... )"
   show IOFunction {name} = []
   show PrimitiveFunction {name} = "primitive " ++ show name
   show (Atom x) = x
